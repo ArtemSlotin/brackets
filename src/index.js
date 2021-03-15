b8f3b62a9d59a4da1cf516bcca89716f439debe4
Artem Slotin <66305683+ArtemSlotin@users.noreply.github.com>
2021-03-15 23:01:15 +0200

    Done Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.join('').replace(/,/g, '');
+    let stack = [];
+
+    for (let symbol of str) {
+        let index = brackets.indexOf(symbol);
+
+        if (index % 2 === 0) {
+            
+            if (symbol === brackets[index + 1] && stack[stack.length - 1] === index) {
+                stack.pop();
+            } else if (symbol === brackets[index + 1] && stack[stack.length - 1] !== index) {
+                stack.push(index);
+            }
+            else {
+                stack.push(index);
+            }
+        } 
+        else {
+            if (stack.pop() !== index-1) {
+                return false;
+            }
+        }
+            
+    }
+    
+    return stack.length === 0;
 }
